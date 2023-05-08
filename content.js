eb94dcf1cef0bf0e265a6b5090d0c77f60ec42e3
Sven Knoerzer <seven@sevenk.org>
2023-05-09 00:42:01 +0200

    Remove extended menu on other pages that wl playlist and remove mutation observer

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
+let mutationObserver;
 function onWatchLaterUrl() {
-    var mutationObserver = new MutationObserver(function (mutations) {
+    mutationObserver = new MutationObserver(function (mutations) {
         const actionsMenuReady = new CustomEvent('actionsMenuReady');
 
         mutations.forEach(function (mutation) {
@@ -16,7 +17,7 @@ function onWatchLaterUrl() {
     document.addEventListener('actionsMenuReady', async () => {
         await customizeActionsMenu();
     });
-    
+
     function waitUntilActionsMenuIsReady() {
         return new Promise((resolve, reject) => {
             // resolve();
@@ -25,19 +26,19 @@ function onWatchLaterUrl() {
                 document.removeEventListener('actionsMenuReady', handleEvent);
                 resolve();
             }
-    
+
             document.addEventListener('actionsMenuReady', handleEvent);
         });
     }
-    
+
     async function customizeActionsMenu() {
         await waitUntilActionsMenuIsReady();
-    
+
         let delComplWatchedAdded = document.querySelector('#delComplWatched') != null;
         if (!delComplWatchedAdded) {
             let delItem = document.querySelectorAll("ytd-menu-service-item-renderer")[document.querySelectorAll("ytd-menu-service-item-renderer").length - 1];
             let text = delItem.querySelector('yt-formatted-string').textContent;
-    
+
             const item = document.createElement('div');
             item.setAttribute('id', 'delComplWatched')
             item.classList.add('wvoytr-action-menu-item');
@@ -46,28 +47,28 @@ function onWatchLaterUrl() {
             item.style.padding = '8px 0';
             const logo = document.createElement('img');
             logo.src = chrome.runtime.getURL("images/icon16.png");
-    
+
             delItem.parentElement.appendChild(item);
-    
+
             let delComplWatchedVideosBtn = document.querySelector("#delComplWatched");
             delComplWatchedVideosBtn.style.display = 'flex';
             delComplWatchedVideosBtn.style.marginTop = '4px';
-    
+
             const logoDiv = document.createElement('div');
             logoDiv.style.marginLeft = '22px';
             logoDiv.style.display = 'flex';
             logoDiv.style.justifyContent = 'center';
             logoDiv.style.alignItems = 'center';
-    
+
             logoDiv.appendChild(logo);
-    
-            const textDiv = document.createElement('div');  
-            textDiv.style.marginLeft = '18px';     
+
+            const textDiv = document.createElement('div');
+            textDiv.style.marginLeft = '18px';
             textDiv.innerHTML = text == 'Gesehene Videos entfernen' ? 'Vollständig gesehene Videos entfernen' : 'Remove completely watched videos';
-            
+
             delComplWatchedVideosBtn.appendChild(logoDiv);
             delComplWatchedVideosBtn.appendChild(textDiv);
-            
+
             delComplWatchedVideosBtn.removeEventListener('tap', () => { }); // ontap gives an exeption but I can't remove it because th event listener comes from the webcomponent ytd-menu-service-item-renderer
             delComplWatchedVideosBtn.addEventListener('click', () => {
                 list = document.querySelector("#contents .ytd-section-list-renderer").querySelector("#contents").querySelector("#contents").querySelectorAll("ytd-playlist-video-renderer")
@@ -77,7 +78,7 @@ function onWatchLaterUrl() {
             observeMenuPopupRenderer();
         }
     }
-    
+
     function observeMenuPopupRenderer() {
         const menuPopupRendererObserver = new MutationObserver((mutations) => {
             for (const mutation of mutations) {
@@ -89,7 +90,7 @@ function onWatchLaterUrl() {
                 }
             }
         });
-    
+
         const menuPopupRenderer = document.querySelector('ytd-menu-popup-renderer');
         if (menuPopupRenderer) {
             menuPopupRendererObserver.observe(menuPopupRenderer, {
@@ -98,7 +99,7 @@ function onWatchLaterUrl() {
             });
         }
     }
-    
+
 
     mutationObserver.observe(document.documentElement, {
         attributes: true,
@@ -108,14 +109,14 @@ function onWatchLaterUrl() {
         attributeOldValue: true,
         characterDataOldValue: true
     });
-    
-    
+
+
     function wait(ms) {
         return new Promise(resolve => { setTimeout(resolve, ms); });
     }
-    
+
     async function remove() {
-    
+
         for (let el of list) {
             let pgBar = el.querySelectorAll("#content")[0].querySelector("#progress");
             if (pgBar) {
@@ -128,14 +129,14 @@ function onWatchLaterUrl() {
                             document.querySelector("ytd-menu-popup-renderer").querySelector("tp-yt-paper-listbox").children[2].click();
                         }
                     }, 100);
-    
+
                     await wait(1000);
                 };
             }
         }
         document.querySelector("ytd-popup-container tp-yt-iron-dropdown").style.display = 'none';
     }
-    
+
 
 };
 
@@ -154,6 +155,16 @@ function observeTitleChanges(callback) {
 function checkAndUpdate() {
     if (window.location.href === "https://www.youtube.com/playlist?list=WL") {
         onWatchLaterUrl();
+    } else {
+        if (mutationObserver) {
+            mutationObserver.disconnect();
+        }
+
+        // Remove menu item from other pages
+        const delComplWatchedVideosBtn = document.querySelector("#delComplWatched");
+        if (delComplWatchedVideosBtn) {
+            delComplWatchedVideosBtn.remove();
+        }
     }
 }
 
